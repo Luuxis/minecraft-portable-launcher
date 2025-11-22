8b3d3a13c3f88a7e855f53a1fbc303e9c2f1f37d
Luuxis <61792917+luuxis@users.noreply.github.com>
2025-11-22 21:06:54 +0100

    Migrate project from Rust to Go
    Removed all Rust-related files and dependencies, including Cargo configuration, source code, and assets. Added initial Go project structure with main.go, config/config.go, and go.mod. Updated .gitignore to reflect new project files.

---
+++ config/config.go
@@ -0,0 +1,5 @@
+package config
+
+var Darwin = "https://redstone-launcher.mojang.com/release/v2/products/launcher/eb53bc69b6bf170aa6321cf422d7f43b33a3470e/macos.json"
+var Windows = "https://launchermeta.mojang.com/v1/products/launcher/d03cf0cf95cce259fa9ea3ab54b65bd28bb0ae82/windows-x86.json"
+var Linux = "https://redstone-launcher.mojang.com/release/v2/products/launcher/75c4b4bddedcc2b1b10af1a720b8dc7a9a18c8d4/linux.json"
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "minecraft/config"
+)
+
+func main() {
+    fmt.Println(config.Darwin)
+    fmt.Println(config.Windows)
+    fmt.Println(config.Linux)
+}
